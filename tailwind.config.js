54742eabe25597d651589f92c32d1cf95db8c732
HUU HUONG NGUYEN <airoolsmastery2026@gmail.com>
2026-07-22 11:52:40 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          navy: "#0B132B",
+          orange: "#FF5722",
+        }
+      }
+    },
+  },
+  plugins: [],
+};
